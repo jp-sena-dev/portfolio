a279c31e7e3bd54d0072dafcfa824477c593e1a5
joaopedr0sena <joaopedrosena1997@gmail.com>
2024-01-18 16:22:34 -0300

    :sparkles: update: navigation bar

--- src/components/header/components/nav.tsx
+++ src/components/header/components/nav.tsx
@@ -1,8 +1,6 @@
-'use client';
-
 import { useEffect } from 'react';
 import { useAnimate } from 'framer-motion';
-import { Button } from './ui/button';
+import { Button } from '@/components/ui/button';
 
 interface NavProps {
   selectedSection: string;
@@ -15,9 +13,9 @@ export function Nav({ selectedSection, setSelectedSection }: NavProps) {
   useEffect(() => {
     if (selectedSection === 'home') {
       animate('nav', {
-        position: 'fixed',
-        top: '132px',
-        left: 62,
+        position: 'absolute',
+        top: '50px',
+        left: 0,
         transform: 0,
       });
     } else {
@@ -30,9 +28,9 @@ export function Nav({ selectedSection, setSelectedSection }: NavProps) {
     }
   }, [animate, selectedSection]);
   return (
-    <div ref={escope}>
-      <nav>
-        <div className="text-xl text-muted-foreground">
+    <div>
+      <nav className={`z-[51] flex justify-center transition-all md:hidden w-screen mx-[-16px] fixed top-${selectedSection === 'home' ? '18' : '4'}`}>
+        <div>
           <Button variant="ghost">
             <a href="#home" className="relative font-bold text-xl" onClick={() => setSelectedSection('home')}>
               HOME
@@ -53,7 +51,30 @@ export function Nav({ selectedSection, setSelectedSection }: NavProps) {
           </Button>
         </div>
       </nav>
-      {/* </motion.div> */}
+      <div ref={escope} className="hidden md:block ">
+        <nav className={selectedSection === 'home' ? 'md:m-0 flex z-[51]' : 'w-max flex z-[51]'}>
+          <div>
+            <Button variant="ghost">
+              <a href="#home" className="relative font-bold text-xl" onClick={() => setSelectedSection('home')}>
+                HOME
+                <div className={`absolute transition-all ${selectedSection === 'home' ? 'w-full' : 'w-0'} h-[12%] bg-action left-0 bottom-0`} />
+              </a>
+            </Button>
+            <Button variant="ghost">
+              <a href="#portfolio" className="relative font-bold text-xl" onClick={() => setSelectedSection('portfolio')}>
+                PORTFOLIO
+                <div className={`absolute transition-all ${selectedSection === 'portfolio' ? 'w-full' : 'w-0'} h-[12%] bg-action left-0 bottom-0`} />
+              </a>
+            </Button>
+            <Button variant="ghost">
+              <a className="relative font-bold text-xl" href="#about" onClick={() => setSelectedSection('about')}>
+                ABOUT
+                <div className={`absolute transition-all ${selectedSection === 'about' ? 'w-full' : 'w-0'} h-[12%] bg-action left-0 bottom-0`} />
+              </a>
+            </Button>
+          </div>
+        </nav>
+      </div>
     </div>
   );
 }
