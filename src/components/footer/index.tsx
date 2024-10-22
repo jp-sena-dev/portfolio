1514afbba0c7bace3f0c9f71ef341f86c55cdd30
jp-sena-dev <joaopedrosena1997@gmail.com>
2024-10-22 10:27:55 -0300

    :lipstick: update: footer media query

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,48 +1,15 @@
 export function Footer() {
   return (
-    <footer className="bg-white mt-28 py-8 md:px-20 px-4">
-      <div className="mt-20 flex flex-col md:flex-row justify-between">
-        <div className="font-medium text-center md:text-left">
-          <p className="text-2xl md:text-5xl mb-5 md:mb-10">
-            Vamos trocar uma ideia?
-            <br />
-            Adoraria
-            <span className="text-action"> ouvir de você</span>
-            !
-          </p>
-          <p className="text-3xl md:text-6xl mb-10 md:mb-0">
-            Conecte-se
-            {' '}
-            <a rel="noreferrer" target="_blank" href="https://www.linkedin.com/in/jp-sena-dev/" className="text-action underline hover:text-action-100 trnasition">comigo</a>
-            !
-          </p>
-        </div>
-        <div>
-          <div className="flex flex-col items-center text-text-100">
-            <p className="md:text-2xl font-medium text-text">Redes Sociais</p>
-            <a rel="noreferrer" target="_blank" href="https://www.linkedin.com/in/jp-sena-dev/" className="hover:text-black transition">LinkedIn</a>
-            <a rel="noreferrer" target="_blank" href="https://github.com/jp-sena-dev" className="hover:text-black transition">Github</a>
-            <a rel="noreferrer" target="_blank" href="https://www.instagram.com/joao_sena1410/" className="hover:text-black transition">Instagram</a>
-          </div>
-          <div className="flex flex-col items-center mt-10 text-text-100">
-            <p className="md:text-2xl font-medium text-text">Contato</p>
-            <p className="hover:text-black transition">email: joaopedrosena1410@gmail.com</p>
-          </div>
-        </div>
-      </div>
-      <hr className="my-14" />
-      <div className="flex items-center justify-between w-full">
-        <a href="https://www.linkedin.com/in/jp-sena-dev/" target="_blank" rel="noreferrer">
-          <h1 className="font-bold md:text-xl lg:text-3xl">
-            JP-SENA
-            <span className="text-action">.</span>
-            DEV
-          </h1>
-        </a>
-        <p className="text-text text-[8px] md:text-[11px] text-text-100">
-          <span className="text-action">developed by</span>
-          : @João Pedro sena de Moura
-        </p>
+    <footer className="bg-white mt-28 py-12 md:px-10 xl:px-20 px-4 flex items-center flex-col-reverse gap-8 md:gap-0 md:flex-row justify-between">
+      <p className="text-text text-sm text-text-100 cursor-default">
+        <span className="text-action">developed by</span>
+        : @João Pedro sena de Moura
+      </p>
+      <div className="flex gap-4 sm:gap-8 text-text-100">
+        <a rel="noreferrer" target="_blank" href="https://www.linkedin.com/in/jp-sena-dev/" className="hover:text-black transition">LinkedIn</a>
+        <a rel="noreferrer" target="_blank" href="https://github.com/jp-sena-dev" className="hover:text-black transition">Github</a>
+        <a rel="noreferrer" target="_blank" href="https://www.instagram.com/joao_sena1410/" className="hover:text-black transition">Instagram</a>
+        <a href="mailto:joaopedrosena1410@gmail.com?subject=Oportunidade%20de%20Trabalho&body=Olá%20João,%0D%0A%0D%0AVi%20seu%20perfil%20e%20gostaria%20de%20conversar%20sobre%20uma%20oportunidade.%0D%0A%0D%0AAtenciosamente,%0D%0A[Seu%20Nome]" className="hover:text-black transition">email</a>
       </div>
     </footer>
   );
